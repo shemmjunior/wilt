2c01125e7ad0191d723405ad239649568de9b4c3
Shemm Junior <juniorshemm@gmail.com>
2021-06-14 19:25:55 +0300

    Improved UI Responsiveness

--- src/app/wilt/home/home.component.ts
+++ src/app/wilt/home/home.component.ts
@@ -5,39 +5,84 @@ import { Router, ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  items = [{ title: 'Profile' }, { title: 'Logout' }];
 
-  items = [
+  minimized = [
+    { title: 'Home' },
+    { title: 'Explore' },
+    { title: 'Categories' },
+    { title: 'Add Wilt' },
+    { title: 'My Wilts' },
+    { title: 'Favourites' },
     { title: 'Profile' },
     { title: 'Logout' },
   ];
 
-  constructor(private searchService: NbSearchService, private menuService: NbMenuService, private router: Router, private route: ActivatedRoute) { }
+  constructor(
+    private searchService: NbSearchService,
+    private menuService: NbMenuService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.menuService.onItemClick().subscribe((event) => {
       if (event.item.title === 'Profile') {
-        console.log('Profile Clicked')
-        this.openProfilePage()
+        this.openProfilePage();
       } else if (event.item.title === 'Logout') {
-        console.log('Logut')
-        this.logout()
+        this.logout();
+      } else if (event.item.title === 'Home') {
+        this.openHomePage();
+      } else if (event.item.title === 'Explore') {
+        this.openExplorePage();
+      } else if (event.item.title === 'Categories') {
+        this.openCategoriesPage();
+      } else if (event.item.title === 'Add Wilt') {
+        this.openAddWiltPage();
+      } else if (event.item.title === 'My Wilts') {
+        this.openMyWiltsPage();
+      } else if (event.item.title === 'Favourites') {
+        this.openFavouritesPage();
       }
-    })
+    });
   }
 
   openProfilePage() {
-    this.router.navigate(['./profile'], { relativeTo: this.route })
+    this.router.navigate(['./profile'], { relativeTo: this.route });
+  }
+
+  openHomePage() {
+    this.router.navigate(['./wilts'], { relativeTo: this.route });
+  }
+
+  openExplorePage() {
+    this.router.navigate(['./explore'], { relativeTo: this.route });
+  }
+
+  openCategoriesPage() {
+    this.router.navigate(['./categories'], { relativeTo: this.route });
+  }
+
+  openMyWiltsPage() {
+    this.router.navigate(['./own'], { relativeTo: this.route });
+  }
+
+  openAddWiltPage() {
+    this.router.navigate(['./add'], { relativeTo: this.route });
+  }
+
+  openFavouritesPage() {
+    this.router.navigate(['./favourites'], { relativeTo: this.route });
   }
 
   logout() {
-    this.router.navigate(['../'])
+    this.router.navigate(['../']);
   }
 
   closeSearch(): void {
     this.searchService.deactivateSearch('');
   }
-
 }
